158693a9f41f0cb768d2f9cc35a2dcb9c9d44c97
Antony Grace <graceantony202@gmail.com>
2025-12-24 10:27:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-455145078-afe82",
+  "appId": "1:386024054896:web:b3375fa71622b1555e54b5",
+  "apiKey": "AIzaSyBWLfHX-wPbwkhbgwrpR9zVyBzwlHeoPP0",
+  "authDomain": "studio-455145078-afe82.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "386024054896"
+};
